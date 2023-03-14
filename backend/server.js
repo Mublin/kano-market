c017b09c71096418fac9f20c3052f2c3151696d4
Mubleen <98647270+Mubleen@users.noreply.github.com>
2023-03-14 16:02:02 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,9 +34,9 @@ app.use((err, req, res, next)=>{
     res.status(500).send({message: err.message})
 })
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.get('*', (req, res) =>
-res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
 
 
