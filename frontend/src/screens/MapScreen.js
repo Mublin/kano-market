2c2229b6522176ad5dbcd2d24f2f7b6535d8c615
Mubleen <98647270+Mubleen@users.noreply.github.com>
2023-03-14 14:05:30 +0100

    Delete frontend/src directory

--- frontend/src/screens/MapScreen.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function MapScreen() {
-  return (
-    <div>MapScreen</div>
-  )
-}
--- frontend/src/utils.js
+++
@@ -1,4 +0,0 @@
-export const getError = (error) => {
-    return error.response && error.response.data.message ? (
-        error.response.data.message) : error.message
-}
