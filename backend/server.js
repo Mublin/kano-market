5505a3bce4389d2ba79e149bdc9055d2e556601d
Mubleen <98647270+Mubleen@users.noreply.github.com>
2023-03-14 15:22:40 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use("/api/orders", orderRouter)
 app.use((err, req, res, next)=>{
     res.status(500).send({message: err.message})
 })
-
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('*', (req, res) =>
 res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
